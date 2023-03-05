ad72fff6e0a6d10232b27ca930920c9de420515d
Phillip Ma <phillipma@g.ucla.edu>
2023-03-05 15:57:34 -0800

    added map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import Map from '../assets/Map.png';
 
 const MapScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style= {{alignItems: 'center'}}>
+        <Text style={{marginTop: 20}}>HandiWalk</Text>
+        <Image style={styles.map_style}source={Map}/>
+      </View>
+      <View>
+       <Text>TEST</Text>    
+       <Text>HELLO</Text>  
+      </View>
+      
       <Button
         title="Find a new route"
         onPress={() => navigation.navigate('Home')}
@@ -17,10 +25,21 @@ const MapScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6495ed',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-between",
+    textAlignVertical: 'center'
   },
+  phillip: {
+    flex:1,
+    backgroundColor: 'red',
+  },
+  map_style: {
+    width: 360,
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 18,
+  }
 });
 
 export default MapScreen
