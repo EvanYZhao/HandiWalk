0716cb0d4d8aec53f4deb0a4d351be5cb083d14d
Evan Zhao <zevany16@gmail.com>
2023-03-05 16:26:22 -0800

    Homepage Style, Inputs, Parameter Passing
    * Styled the homepage to contain brand name
    * Added input components and refactored into its own component within a newly created component folder
      - Input boxes could use styling improvements (i.e. stop it from resizing whenever a user types)
    * Passed inputs in the home page to the map page

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,26 +1,28 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-const MapScreen = ({ navigation }) => {
+const MapScreen = ({ navigation, route }) => {
+  const {start, destination} = route.params; //Start and Destination Locations From User
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Button
         title="Find a new route"
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => navigation.navigate("Home")}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-export default MapScreen
+export default MapScreen;
